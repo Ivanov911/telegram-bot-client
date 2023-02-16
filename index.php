e4648b8ca756793c74154b3eed0ee20b22d971b3
Ivanov_S <sergeialex89@mail.ru>
2023-02-16 21:46:33 +0300

    8

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ $telegramBotClient = new TelegramBotClient('6201295676:AAGLTkQUJJk5NAQ8XYFDAjdts
 
 // var_dump($telegramBotClient->getUpdates());
 $str = (string) readline('Enter text: ');
-// print_r($telegramBotClient->sendMessage(699873958, $str));
+print_r($telegramBotClient->sendMessage(699873958, $str));
 
 
 // echo $telegramBotClient->getUpdates()['result'][0]['message']['chat']['id']; чтоб узнать ID
 
-for ($i=1; $i<100; $i++) {
-    print_r($telegramBotClient->sendMessage(699873958, $str));
-}
+// for ($i=1; $i<100; $i++) {
+//     print_r($telegramBotClient->sendMessage(699873958, $str));
+// }
